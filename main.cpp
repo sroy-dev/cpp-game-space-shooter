cd67aa3358b4cb1fefa99ab0b343a0255aab9c79
sroy-me <44993145+sroy-me@users.noreply.github.com>
2023-07-29 19:49:59 +0600

    game over

--- main.cpp
+++ main.cpp
@@ -11,10 +11,16 @@ private:
     int selectedMenuItem = 1;
     string selectedMenu = "startMenu"; // startMenu, pausedMenu, none
 
-    int n_Gun = 0;
-    int shipSpeed = 4;
     int y = 30, m = y * 2;
-    int gunPts[1024];
+    int n_BossBullets = 0;
+    int bossBulletsPos[1024];
+
+    int bulletgap = 30;
+    int bulletPos[1024];
+    int n_bullet = 0;
+
+
+    int shipSpeed = 8;
     int shipW = 70, shipH = 80;
     int shipPos[4] = {
         0,
@@ -22,7 +28,20 @@ private:
         0,
         0,
     };
-    int objectPts[81] = {
+
+    int score = 0;
+    int isRunnig = 0;
+    int life = 5;
+    int isGameOver = 0;
+
+    int safeLine = 410;
+    int totalEnemyShip = 17*2+1;
+    int boss_hp = 5;
+
+
+    int objectSpeed = 4;
+    int temp=0;
+    int enemyShipsPos[81] = {
         40,
         100,
         70,
@@ -212,27 +231,93 @@ private:
         cleardevice();
         while(1){
 
+            if(isGameOver){
+                break;
+            }
+
             if(GetAsyncKeyState(VK_RIGHT)){
-//                if(shipPts[6] < 630){
-//                    for(int i = 0 ; i < 24 ; i = i + 2){
-//                        shipPts[i]+=shipSpeed;
-//                    }
-//                }
+                if(shipPos[2] < this->screenWidth){
+                    shipPos[0]+=shipSpeed;
+                    shipPos[2]+=shipSpeed;
+                }
             }else if(GetAsyncKeyState(VK_LEFT)){
-//                if(shipPts[16] > 0){
-//                    for(int i = 0 ; i < 24 ; i = i + 2){
-//                        shipPts[i]-=shipSpeed;
-//                    }
-//                }
+                if(shipPos[0] > 0){
+                    shipPos[0]-=shipSpeed;
+                    shipPos[2]-=shipSpeed;
+                }
+            }else if(GetAsyncKeyState(VK_SPACE)){
+                bulletPos[n_bullet*2] = shipPos[0] + 35; //x-coordinate same as ship center pt
+                bulletPos[n_bullet*2+1] = shipPos[1] - 20;//y-coordinate
+                n_bullet++;
+                n_bullet = n_bullet%1024;
+            }
+
+            //Boss Firing bullets
+            int r = rand()%100;
+            if(enemyShipsPos[35]>0){
+                r = rand()%100;
+                if(r>95){
+                    bossBulletsPos[n_BossBullets*2] = 300; //x-coordinate same as ship center pt
+                    bossBulletsPos[n_BossBullets*2+1] = 60;//y-coordinate
+                    n_BossBullets++;
+                    n_BossBullets = n_BossBullets%1024;
+                }
             }
 
             drawShip();
+            drawBullets();
 
+            drawEnemyShips();
+            drawBossBullets();
+
+            checkCollision();
+            checkCollisionShip();
+            setfillstyle(SOLID_FILL,BLACK);
+            bar(0,0,640,30);
+            drawScore(score);
+            drawLife();
+
+//            cout << "-----" <<endl;
 
             delay(40);
             cleardevice();
         }
-//        drawGun();
+
+        char msg[20];
+        if(isGameOver == 1) {
+            snprintf(msg, sizeof(msg), "Your Score:%d", score);
+            while(1) {
+                setcolor(GREEN);
+                settextstyle(8, HORIZ_DIR, 1);
+                outtextxy(240, 200, "You Won");
+                setcolor(WHITE);
+                outtextxy(240, 220, msg);
+                outtextxy(240, 240, "Press ESC to Exit");
+                if(GetAsyncKeyState(VK_ESCAPE)) {
+//                    closegraph();
+                    return;
+                }
+                delay(40);
+                cleardevice();
+            }
+        } else {
+            snprintf(msg, sizeof(msg), "Your Score:%d", score);
+            while(1) {
+                //PlaySoundW(TEXT(""),NULL,SND_NOWAIT);
+                setcolor(RED);
+                settextstyle(8, HORIZ_DIR, 1);
+                outtextxy(240, 200, "Game Over");
+                setcolor(WHITE);
+                outtextxy(240, 220, msg);
+                outtextxy(240, 240, "Press ESC to Exit");
+                if(GetAsyncKeyState(VK_ESCAPE)) {
+//                    closegraph();
+                    return;
+                }
+                delay(60);
+                cleardevice();
+            }
+        }
 
     }
 
@@ -240,24 +325,124 @@ private:
         readimagefile("assets/spaceShip.jpg",shipPos[0],shipPos[1],shipPos[2],shipPos[3]);
     }
 
-//    void drawGun(){
-//        if(objectPts[35]>0){
-//            int length = shipPts[1] - 20;
-//            float del_X = shipPts[0] - 280 ;
-//            del_X = del_X/length;
-//            int del_Y = 1;
-//
-//            for(int i = 0,j=0 ; i < n_Gun ; i ++,j=j+2){
-//                setcolor(RED);
-//               // floodfill(gunPts[j],gunPts[j+1],RED);
-//                circle(gunPts[j],gunPts[j+1],5);
-//                gunPts[j] += del_X*5;
-//                gunPts[j+1] += del_Y*5;
-//            }
-//        }
-//    }
+    void drawBullets(){
+        setcolor(YELLOW);
+        for(int i = 0, j=0 ; i < n_bullet ; i++, j=j+2){
+//            readimagefile("assets/bullet.jpg",bulletPos[j]-3,bulletPos[j+1],bulletPos[j]+3,bulletPos[j+1]+12);
+            rectangle(bulletPos[j]-2,bulletPos[j+1],bulletPos[j]+2,bulletPos[j+1]+8);
+            bulletPos[j+1] -= bulletgap;
+//            cout << bulletPos[i] << endl;
+        }
+    }
+
+    void drawEnemyShips(){
+        for(int i = 0,j=0 ; i < 17*2+1; i++,j = j + 2){
+            if(i==17){
+//                readimagefile("assets/enemy.jpg",enemyShipsPos[j],enemyShipsPos[j+1],enemyShipsPos[j]+40,enemyShipsPos[j+1]+40);//for filled rectangle
+                setfillstyle(SOLID_FILL,RED);
+                bar(enemyShipsPos[j],enemyShipsPos[j+1],enemyShipsPos[j]+40,enemyShipsPos[j+1]+40);//for filled rectangle
+            }else{
+//                readimagefile("assets/enemy.jpg",enemyShipsPos[j],enemyShipsPos[j+1],enemyShipsPos[j]+20,enemyShipsPos[j+1]+20);//for filled rectangle
+
+                setfillstyle(SOLID_FILL,WHITE);
+                bar(enemyShipsPos[j],enemyShipsPos[j+1],enemyShipsPos[j]+20,enemyShipsPos[j+1]+20);//for filled rectangle
+            }
+            if(temp==20){
+                if(i!=17){
+                    enemyShipsPos[j+1] += objectSpeed;
+                }
+            }
+        }
+        if(temp==20){
+            temp=0;
+        }
+        temp++;
+    }
+
+    void drawBossBullets(){
+        if(enemyShipsPos[35]>0){
+            int length = 400;
+            float del_X = 50 ;
+            del_X = del_X/length;
+            int del_Y = 1;
+
+            for(int i = 0,j=0 ; i < n_BossBullets ; i ++,j=j+2){
+                setcolor(RED);
+                circle(bossBulletsPos[j],bossBulletsPos[j+1],5);
+                bossBulletsPos[j] += del_X*5;
+                bossBulletsPos[j+1] += del_Y*5;
+            }
+        }
+    }
+
+    void drawScore(int score){
+        setcolor(YELLOW);
+        char scoreResult[10];
+        settextstyle(9,HORIZ_DIR,1);
+        sprintf(scoreResult,"Score: %d",score);
+        outtextxy(screenWidth - 150,screenHeight - 30,scoreResult);
+    }
+
+    void drawLife(){
+        setcolor(YELLOW);
+        char lifeResult[10];
+        sprintf(lifeResult, "Life:  %d", life*100/5);
+        outtextxy(10,screenHeight - 30,lifeResult);
+    }
 
 
+    void checkCollision(){
+        int width = 20;
+        if(totalEnemyShip<=0){
+            isGameOver = 1;
+        }
+        for(int i = 0,a=0 ; i < 17*2+1 ;i++,a=a+2){
+            if(enemyShipsPos[a+1]>=safeLine-20){
+                isGameOver=2;
+            }
+            for(int j = 0,k=0 ; j < n_bullet ; j++,k=k+2){
+                if(bulletPos[k+1]>0){
+                    if(a==34){
+                        width = 40;
+                    }else{
+                        width = 20;
+                    }
+                    if(enemyShipsPos[a]<=bulletPos[k] && bulletPos[k]<=enemyShipsPos[a]+width){
+                        if(enemyShipsPos[a+1]<=bulletPos[k+1] && bulletPos[k+1]<=enemyShipsPos[a+1]+width){
+                            if(a==34 && boss_hp!=0){
+                                score=score+10;
+                                boss_hp--;
+                                bulletPos[k+1]=-1000;
+                                // enemyShipsPos[a+1]=-1000;
+                            }
+                            else{
+                                enemyShipsPos[a+1] = -1000;
+                                bulletPos[k+1] = -1000;
+                                score = score + 10;
+                                totalEnemyShip--;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void checkCollisionShip(){
+        for(int j = 0,k=0 ; j < n_BossBullets ; j++,k=k+2){
+            if(bossBulletsPos[k+1]>0){
+                if(shipPos[16]<=bossBulletsPos[k] && bossBulletsPos[k]<=shipPos[6]){
+                    if(shipPos[1]<=bossBulletsPos[k+1] && bossBulletsPos[k+1]<=shipPos[7]){
+                        life--;
+                        bossBulletsPos[k+1]=1000;
+                        if(life==0)
+                            isGameOver = 2;
+                    }
+                }
+            }
+        }
+    }
 
 };
 
